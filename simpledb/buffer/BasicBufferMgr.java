c7cda5da1e6639ce86edde7ae4774ad8c4555c81
Sunil D S <sunil3590@gmail.com>
2015-04-26 13:14:01 -0400

    Remove unnecessary imports

--- simpledb/buffer/BasicBufferMgr.java
+++ simpledb/buffer/BasicBufferMgr.java
@@ -1,8 +1,6 @@
 package simpledb.buffer;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
